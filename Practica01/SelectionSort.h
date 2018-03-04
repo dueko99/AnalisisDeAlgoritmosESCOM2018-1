ade1d18cbc1e2131a3d3e926378507f43207bd7a
akotadi <manuel_7795@hotmail.com>
2018-03-04 12:07:19 -0600

    Update SelectionSort.h

--- Practica01/SelectionSort.h
+++ Practica01/SelectionSort.h
@@ -1,14 +1,33 @@
+/*
+Implementación Práctica 01: Pruebas a posteriori (Algoritmos de Ordenamiento)
+Por: 
+Versión: 1.0
 
-void SelectionSort(int *A, int n){
-  for (int k = 0; k < n-1; k++) {
+Descripción: Programa que ordenará por medio del algoritmo de Selección
+  
+Observaciones: 
+*/
+
+
+
+//DEFINICIÓN DE FUNCIONES
+
+/*
+Descripción: Función encargada de ordenar los números por medio del algoritmo de Selección
+Recibe: int * Data (arreglo de números a ordenar), int size (cantidad de números)
+Devuelve: 
+Observaciones: 
+*/
+void SelectionSort(int *Data, int size){
+  for (int k = 0; k < size-1; k++) {
     int p=k;
-    for (int i=k+1; i < n; i++) {
-      if (A[i]<A[p]) {
+    for (int i=k+1; i < size; i++) {
+      if (Data[i]<Data[p]) {
         p=i;
       }
     }
-    int aux=A[p];
-    A[p]=A[k];
-    A[k]=aux;
+    int aux=Data[p];
+    Data[p]=Data[k];
+    Data[k]=aux;
   }
 }
