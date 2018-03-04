e754d4ccf988bb17e34d88eb8f14d44517136f40
akotadi <manuel_7795@hotmail.com>
2018-03-04 12:05:50 -0600

    Update BubbleSort.h

--- Practica01/BubbleSort.h
+++ Practica01/BubbleSort.h
@@ -1,27 +1,53 @@
-#include "stdio.h"
-void SimpleBubbleSort(int *A,int n){
-  for (int i = 0; i < n-1; i++) {
-    for (int j = 0; j < n-1-i; j++) {
-      if(A[j]>A[j+1]){
-        int temp=A[j+1];
-        A[j+1]=A[j];
-        A[j]=temp;
+/*
+Implementación Práctica 01: Pruebas a posteriori (Algoritmos de Ordenamiento)
+Por: 
+Versión: 1.0
+
+Descripción: Programa que ordenará por medio del algoritmo de Burbuja, 
+  tanto simple como optimizada
+  
+Observaciones: 
+*/
+
+
+
+//DEFINICIÓN DE FUNCIONES
+
+/*
+Descripción: Función encargada de ordenar los números por medio del algoritmo de Burbuja Simple
+Recibe: int * Data (arreglo de números a ordenar), int size (cantidad de números)
+Devuelve: 
+Observaciones: 
+*/
+void SimpleBubbleSort(int * Data,int size){
+  for (int i = 0; i < size-1; i++) {
+    for (int j = 0; j < size-1-i; j++) {
+      if(Data[j]>Data[j+1]){
+        int temp=Data[j+1];
+        Data[j+1]=Data[j];
+        Data[j]=temp;
       }
     }
   }
 }
 
-void OptimizedBubbleSort(int *A, int n){
+/*
+Descripción: Función encargada de ordenar los números por medio del algoritmo de Burbuja Optimizada
+Recibe: int * Data (arreglo de números a ordenar), int size (cantidad de números)
+Devuelve: 
+Observaciones: 
+*/
+void OptimizedBubbleSort(int *Data, int size){
   int changes=1;
   int i=0;
-  while((i<n-1)&&(changes!=0)) {
+  while((i<size-1)&&(changes!=0)) {
     changes=0;
-    for (int j = 0; j < n-1-i; j++) {
-      if(A[j]>A[j+1]){
+    for (int j = 0; j < size-1-i; j++) {
+      if(Data[j]>Data[j+1]){
         changes=1;
-        int temp=A[j+1];
-        A[j+1]=A[j];
-        A[j]=temp;
+        int temp=Data[j+1];
+        Data[j+1]=Data[j];
+        Data[j]=temp;
       }
     }
     i++;
