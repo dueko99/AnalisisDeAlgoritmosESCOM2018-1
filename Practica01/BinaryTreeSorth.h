28bd8e0137cba9decac105544f81ca95dfb0fcc0
akotadi <manuel_7795@hotmail.com>
2018-03-04 12:04:46 -0600

    Update BinaryTreeSorth.h

--- Practica01/BinaryTreeSorth.h
+++ Practica01/BinaryTreeSorth.h
@@ -1,9 +1,31 @@
+/*
+Implementación Práctica 01: Pruebas a posteriori (Algoritmos de Ordenamiento)
+Por: 
+Versión: 1.0
+
+Descripción: Programa que ordenará por medio de un Árbol Binario de Búsqueda (ABB)
+	
+Observaciones: 
+*/
+
+
+//LIBRERÍAS
 #include "AuxBinaryTreeFunctions.h"
 
-void BinaryTreeSort(li * Data, li size){
+
+
+//DEFINICIÓN DE FUNCIONES
+
+/*
+Descripción: Función encargada de ordenar los números por medio de un ABB
+Recibe: int * Data (arreglo de números a ordenar), int size (cantidad de números)
+Devuelve: 
+Observaciones: 
+*/
+void BinaryTreeSort(int * Data, int size){
 	arbol_bin BinaryTree;
 	Initialize(&BinaryTree);
-	li i;
+	int i;
 	for (i = 0; i < size; ++i)
 	{
 		Insert(&BinaryTree,Data[i]);
