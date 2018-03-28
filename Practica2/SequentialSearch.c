e3fc2c3d61320ed924dca7a29ed99ea30c0149b4
asdf1234Damian <asdf1234damian@gmail.com>
2018-03-27 19:54:19 -0600

    Implementacion Con Hilos Busqueda Lineal

--- Practica2/SequentialSearch.c
+++ Practica2/SequentialSearch.c
@@ -8,19 +8,19 @@ Observaciones:
 
 /*
 		gcc -o LinealSearch LinealSearch.c
-		./LinealSearch n t v <10millones.txt >> Lineal.txt
+		./LinealSearch n t k <10millones.txt >> Lineal.txt
 		donde n es el tamaño de la busqueda
 		t es el numero de threds
-		v es el valor a busar
+		k es el valor a busar
 */
 
 
 //LIBRERÍAS
-//#include <stdio.h>
 #include <stdlib.h>
 #include "tiempo.h"
 #include "imprimeTiempos.h"
 #include "SequentialSearch.h"
+#include <pthread.h>
 
 
 // FUNCIÓN PRINCIPAL
@@ -38,25 +38,36 @@ Variables usadas en el programa:
 	double stime1: variable que medirá el tiempo de finalización de ejecución del sistema
 	double wtime1: variable que medirá el tiempo de finalización de ejecución real
 */
+
+
+
+
+
+
 int main(int argc,char **argv){
-	int size=atoi(argv[1]);
-  int threads=atoi(argv[2]);
-	int value=atoi(argv[3]);
-	int *Data;
+	size=atoi(argv[1]);
+  threads=atoi(argv[2]);
+	key=atoi(argv[3]);
 	Data=(int*)calloc(size,sizeof(int));//Crea el arreglo de numeros
 	for(int i=0;i<size;i++){
 		scanf("%d",Data+i);
 	}
 	double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medición de tiempos
 	uswtime(&utime0, &stime0, &wtime0);
-	pthread_t *thread;//arreglo para la identificacion de los threads
-	thread = (pthread_t*)malloc(threads*sizeof(pthread_t));//Crea el arreglo de threds
-	if(SequentalSearch(Data,value,size)){
-		printf("Valor encontrado\n");
-	}else{
-		printf("Valor no encontrado\n");
+	pthread_t *threadArr;//arreglo para la identificacion de los threads
+	threadArr = (pthread_t*)malloc(threads*sizeof(pthread_t));//Crea el arreglo de threds
+	for (int i = 0; i < threads; i++){
+		pthread_create(&threadArr[i], NULL, SequentialSearch, (void*)NULL); // se asigna una rutina a cada hilo
+	}
+	for (int i = 0; i < threads; i++) {
+    pthread_join(threadArr[i], NULL); // se comenza la rutina
 	}
 	uswtime(&utime1, &stime1, &wtime1);
+	if (encontrado==1) {
+		printf("Valor encontrado\n");
+	} else {
+		printf("Valor NO encontrado\n");
+	}
 	imprimeTiempos(size,utime0, stime0, wtime0,utime1, stime1, wtime1);
 	free(Data);
 }
--- Practica2/SequentialSearch.h
+++ Practica2/SequentialSearch.h
@@ -1,8 +1,12 @@
-bool SequentalSearch(int* data, int size, int value){
-  for (int i = 0; i < size ; i++) {
-    if (data[i]==value) {
-      return true;
-    }
+int current_thread=0, encontrado = 0,size=0, threads=0,key;
+int* Data;
+
+void* SequentialSearch(void* args){
+  int num = current_thread++;
+  int seccion=num*(size / threads);
+  // se divide el arreglo y se empieza la busqueda
+  for (int i = seccion;i < ((num+1)*(size / threads)); i++){
+      if (Data[i] == key)
+          encontrado =1 ;
   }
-  return false;
 }
